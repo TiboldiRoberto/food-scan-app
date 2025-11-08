5449e72fc4022ad1080cad988ced13998b2a4c77
Roberto <tiboldi.roberto@agilefreaks.com>
2025-11-08 10:18:29 +0200

    Initial project setup

---
+++ FoodScan/App/FoodScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodScanApp.swift
+//  FoodScan
+//
+//  Created by Roberto on 08.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodScan/Features/BarcodeScannerView.swift
@@ -0,0 +1,100 @@
+import SwiftUI
+import AVFoundation
+
+struct BarcodeScannerView: UIViewControllerRepresentable {
+    @Binding var scannedCode: String?
+    @Binding var isScanning: Bool
+
+    func makeUIViewController(context: Context) -> ScannerViewController {
+        let vc = ScannerViewController()
+        vc.delegate = context.coordinator
+        return vc
+    }
+
+    func updateUIViewController(_ uiViewController: ScannerViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, ScannerViewControllerDelegate {
+        var parent: BarcodeScannerView
+
+        init(_ parent: BarcodeScannerView) {
+            self.parent = parent
+        }
+
+        func didFind(code: String) {
+            parent.scannedCode = code
+            parent.isScanning = false
+        }
+
+        func didFail(with error: String) {
+            parent.isScanning = false
+        }
+    }
+}
+
+protocol ScannerViewControllerDelegate: AnyObject {
+    func didFind(code: String)
+    func didFail(with error: String)
+}
+
+class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    weak var delegate: ScannerViewControllerDelegate?
+    var captureSession: AVCaptureSession!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        captureSession = AVCaptureSession()
+
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            delegate?.didFail(with: "Camera indisponibilă")
+            return
+        }
+        guard let videoInput = try? AVCaptureDeviceInput(device: videoCaptureDevice) else {
+            delegate?.didFail(with: "Nu s-a putut accesa camera")
+            return
+        }
+
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            delegate?.didFail(with: "Nu s-a putut adăuga inputul camerei")
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+        if captureSession.canAddOutput(metadataOutput) {
+            captureSession.addOutput(metadataOutput)
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.ean8, .ean13, .code128]
+        } else {
+            delegate?.didFail(with: "Nu s-a putut adăuga outputul")
+            return
+        }
+
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+
+        captureSession.startRunning()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if captureSession.isRunning {
+            captureSession.stopRunning()
+        }
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+        if let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
+           let stringValue = metadataObject.stringValue {
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            delegate?.didFind(code: stringValue)
+        }
+    }
+}
